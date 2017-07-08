61595d1f4eb65419546ec6a47470660c190596b9
user <kionopia@gmail.com>
2017-07-08 19:57:30 +0300

    GetBinaryRepresentation method added

---
+++ NET.S.2017.01.Tsurikova.05/Logic.Tests/DoubleExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Logic;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    class DoubleExtensionTests
+    {
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "1")]
+        [TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "111111111110000000000000000000000000000000000000000000000000000")]
+        public static string GetBinaryRepresentation_Number_Result(double d)
+        {
+            return d.GetBinaryRepresentation();
+        }
+    }
+}
---
+++ NET.S.2017.01.Tsurikova.05/Logic/DoubleExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// provides extension methods for double
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// method for obtaining binary representation of double
+        /// </summary>
+        /// <param name="d">number to be represented</param>
+        /// <returns>binary representation for d</returns>
+        public static string GetBinaryRepresentation(this double d)
+            => Convert.ToString(BitConverter.DoubleToInt64Bits(d), 2);
+    }
+}
