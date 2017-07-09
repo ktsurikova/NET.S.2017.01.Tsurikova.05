2bf5c81eb7b7bb7309d1666d1686d7340878b032
user <kionopia@gmail.com>
2017-07-09 13:42:52 +0300

    Polynomial class&tests added

---
+++ NET.S.2017.01.Tsurikova.05/Logic.Tests/PolynomialTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    class PolynomialTests
+    {
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, 1, ExpectedResult = 3.4)]
+        public static double Indexer_Index_Element(double[] array, int index)
+        {
+            Polynomial p = new Polynomial(array);
+            return p[index];
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, -1)]
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, 3)]
+        public static void Indexer_InvalidIndex_IndexOutOfRangeException(double[] array, int index)
+        {
+            Polynomial p = new Polynomial(array);
+            Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                var d = p[index];
+            });
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, ExpectedResult = 2)]
+        public static double Degree_Array_Degree(double[] array)
+        {
+            Polynomial p = new Polynomial(array);
+            return p.Degree;
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, ExpectedResult = "4,5x^2+3,4x^1+1,2x^0")]
+        [TestCase(new[] { 1.2, 0, 0, 4.5 }, ExpectedResult = "4,5x^3+1,2x^0")]
+        public static string ToString_Array_Result(double[] array)
+        {
+            Polynomial p = new Polynomial(array);
+            return p.ToString();
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 4.5 }, ExpectedResult = false)]
+        [TestCase(new[] { 1.2, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = false)]
+        public static bool Equals_TwoArray_Result(double[] array, double[] other)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            return p.Equals(p2);
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 4.5 }, ExpectedResult = false)]
+        [TestCase(new[] { 1.2, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = false)]
+        public static bool Equality_TwoArray_Result(double[] array, double[] other)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            return p == p2;
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = false)]
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 4.5 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.2, 4.5 }, new[] { 1.2, 3.4, 4.5 }, ExpectedResult = true)]
+        public static bool InEquality_TwoArray_Result(double[] array, double[] other)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            return p != p2;
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 3.4, 4.5 }, new[] { 2.4, 6.8, 9 } , ExpectedResult = true)]
+        [TestCase(new[] { 1.0, 3.4, 4.5 }, new[] { 1.2, 4.5 }, new[] { 2.2, 7.9, 4.5 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.2, 4.5 }, new[] { 1.0, 3.4, 4.5 }, new[] { 2.2, 7.9, 4.5 }, ExpectedResult = true)]
+        public static bool OperatorSum_TwoArray_Result(double[] array, double[] other, double[] result)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            Polynomial p3 = new Polynomial(result);
+            return p3 == (p + p2);
+        }
+
+        [Test]
+        public static void OperatorSum_PolynomialFirst_ArgumentNullException()
+        {
+            Polynomial p = new Polynomial(2.3, 4.4);
+            Assert.Throws<ArgumentNullException>(() => p = p + null);
+        }
+
+        [Test]
+        public static void OperatorSum_PolynomialSecond_ArgumentNullException()
+        {
+            Polynomial p = new Polynomial(2.3, 4.4);
+            Assert.Throws<ArgumentNullException>(() => p = null + p);
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 3.4, 4.5 }, new[] { 0.0, 0.0, 0.0 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.0, 3.4, 4.5 }, new[] { 1.2, 4.5 }, new[] { -0.2, -1.1, 4.5 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.2, 4.5 }, new[] { 1.0, 3.4, 4.5 }, new[] { 0.2, 1.1, -4.5 }, ExpectedResult = true)]
+        public static bool OperatorDifference_TwoArray_Result(double[] array, double[] other, double[] result)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            Polynomial p3 = new Polynomial(result);
+            return p3 == (p - p2);
+        }
+
+        [TestCase(new[] { 1.2, 3.4, 4.5 }, new[] { 1.2, 4.5 }, new[] { 1.44, 9.48, 20.7, 20.25 }, ExpectedResult = true)]
+        public static bool OperatorMultiplication_TwoArray_Result(double[] array, double[] other, double[] result)
+        {
+            Polynomial p = new Polynomial(array);
+            Polynomial p2 = new Polynomial(other);
+            Polynomial p3 = new Polynomial(result);
+            return p3 == (p * p2);
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 5.0 }, 1.0, 9.0)]
+        public static void ValueAt_Array_Result(double[] array, double value, double result)
+        {
+            Polynomial p = new Polynomial(array);
+            Assert.AreEqual(p.ValueAt(value), result, 0.0001); 
+        }
+    }
+}
---
+++ NET.S.2017.01.Tsurikova.05/Logic/Polynomial.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Polynomial : IEquatable<Polynomial>
+    {
+        private readonly double[] coefficients;
+
+        #region ctors
+
+        /// <summary>
+        /// ctor for params double[]
+        /// </summary>
+        /// <param name="array">array of coefficients</param>
+        /// <exception cref="ArgumentNullException">throws when array is null</exception>
+        public Polynomial(params double[] array)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException($"{nameof(array)} is null");
+            coefficients = new double[array.Length];
+            Array.Copy(array, coefficients, array.Length);
+        }
+        
+        /// <summary>
+        /// ctor for polynomial
+        /// </summary>
+        /// <param name="p">polynomial</param>
+        /// <exception cref="ArgumentNullException">throws when p is null</exception>
+        public Polynomial(Polynomial p)
+        {
+            if (ReferenceEquals(p, null)) throw new ArgumentNullException($"{nameof(p)} is null");
+            coefficients = new double[p.Degree + 1];
+            Array.Copy(p.coefficients, coefficients, p.Degree + 1);
+        }
+
+        /// <summary>
+        /// ctor for IEnumerable
+        /// </summary>
+        /// <param name="other">ienumeration of coefficients</param>
+        /// <exception cref="ArgumentNullException">throws when other is null</exception>
+        public Polynomial(IEnumerable<double> other)
+        {
+            if (ReferenceEquals(other, null)) throw new ArgumentNullException($"{nameof(other)} is null");
+            coefficients = new double[other.Count()];
+            int i = 0;
+            foreach (var item in other)
+            {
+                coefficients[i++] = item;
+            }
+        }
+
+        #endregion
+
+        #region Indexer
+
+        public double this[int i]
+        {
+            get
+            {
+                if (i < 0 || i > Degree) throw new IndexOutOfRangeException($"{nameof(i)} is invalid");
+                return coefficients[i];
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsReadOnly => true;
+
+        /// <summary>
+        /// degree of polynomial
+        /// </summary>
+        public int Degree => coefficients.Length - 1;
+
+        #endregion
+
+        #region Override methods
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = Degree; i >= 0; i--)
+            {
+                if (coefficients[i] == 0) continue;
+                if (i != 0)
+                    sb.Append($"{coefficients[i]}x^{i}+");
+                else
+                    sb.Append($"{coefficients[i]}x^{i}");
+            }
+
+            return sb.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return coefficients.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Polynomial)) return false;
+            return Equals((Polynomial)obj);
+        }
+
+        #endregion
+
+        #region operators
+
+        public static bool operator ==(Polynomial p1, Polynomial p2)
+        {
+            if (ReferenceEquals(p1, null) && ReferenceEquals(p2, null)) return true;
+            if (ReferenceEquals(p1, null)) return false;
+            if (ReferenceEquals(p2, null)) return false;
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Polynomial p1, Polynomial p2) => !(p1 == p2);
+
+        public static Polynomial operator +(Polynomial p1, Polynomial p2) => Add(p1, p2);
+
+        public static Polynomial operator -(Polynomial p1, Polynomial p2) => Subtraction(p1, p2);
+
+        public static Polynomial operator *(Polynomial p1, Polynomial p2) => Multiplication(p1, p2);
+
+        #endregion
+
+        #region basic operations
+
+        /// <summary>
+        /// calculates sum of 2 polynomial
+        /// </summary>
+        /// <param name="p1">first summand</param>
+        /// <param name="p2">second summand</param>
+        /// <returns>new polynomial for sum</returns>
+        public static Polynomial Add(Polynomial p1, Polynomial p2)
+        {
+            if (ReferenceEquals(p1, null)) throw new ArgumentNullException($"{nameof(p1)} is null");
+            if (ReferenceEquals(p2, null)) throw new ArgumentNullException($"{nameof(p2)} is null");
+
+            double[] array = new double[Math.Max(p1.Degree + 1, p2.Degree + 1)];
+
+            int i;
+            for (i = 0; i <= Math.Min(p1.Degree, p2.Degree); i++)
+            {
+                array[i] = p1[i] + p2[i];
+            }
+
+            if (p1.Degree > p2.Degree)
+            {
+                for (; i <= p1.Degree; i++)
+                    array[i] = p1[i];
+            }
+            if (p2.Degree > p1.Degree)
+            {
+                for (; i <= p2.Degree; i++)
+                    array[i] = p2[i];
+            }
+
+            return new Polynomial(array);
+        }
+
+        /// <summary>
+        /// calculates difference between 2 polynomial
+        /// </summary>
+        /// <param name="p1">minuend</param>
+        /// <param name="p2">subtrahend</param>
+        /// <returns>new polynomial for differnce</returns>
+        public static Polynomial Subtraction(Polynomial p1, Polynomial p2)
+        {
+            if (ReferenceEquals(p1, null)) throw new ArgumentNullException($"{nameof(p1)} is null");
+            if (ReferenceEquals(p2, null)) throw new ArgumentNullException($"{nameof(p2)} is null");
+
+            double[] array = new double[Math.Max(p1.Degree + 1, p2.Degree + 1)];
+
+            int i;
+            for (i = 0; i <= Math.Min(p1.Degree, p2.Degree); i++)
+            {
+                array[i] = p1[i] - p2[i];
+            }
+
+            if (p1.Degree > p2.Degree)
+            {
+                for (; i <= p1.Degree; i++)
+                    array[i] = p1[i];
+            }
+            if (p2.Degree > p1.Degree)
+            {
+                for (; i <= p2.Degree; i++)
+                    array[i] = -p2[i];
+            }
+
+            return new Polynomial(array);
+        }
+
+        /// <summary>
+        /// calculates product of 2 polynomial
+        /// </summary>
+        /// <param name="p1">first polynomial</param>
+        /// <param name="p2">second polynomial</param>
+        /// <returns>new polynomial for product</returns>
+        public static Polynomial Multiplication(Polynomial p1, Polynomial p2)
+        {
+            if (ReferenceEquals(p1, null)) throw new ArgumentNullException($"{nameof(p1)} is null");
+            if (ReferenceEquals(p2, null)) throw new ArgumentNullException($"{nameof(p2)} is null");
+
+            double[] array = new double[p1.Degree + p2.Degree + 1];
+
+            for (int i = 0; i <= p1.Degree; i++)
+            {
+                for (int j = 0; j <= p2.Degree; j++)
+                {
+                    array[i + j] += p1[i] * p2[j];
+                }
+            }
+
+            return new Polynomial(array);
+        }
+
+        /// <summary>
+        /// calculates value of polynomial for given value
+        /// </summary>
+        /// <param name="value">value for substituring</param>
+        /// <returns>obtaining value</returns>
+        public double ValueAt(double value)
+        {
+            double result = 0;
+            for (int i = 0; i <= Degree; i++)
+            {
+                result += coefficients[i] * Math.Pow(value, i);
+            }
+            return result;
+        } 
+
+        #endregion
+
+        #region interface implementation
+
+        public bool Equals(Polynomial other)
+        {
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (Degree != other.Degree) return false;
+
+            for (int i = 0; i < Degree; i++)
+            {
+                if (!(Math.Abs(coefficients[i]- other.coefficients[i])<0.0001)) return false;
+            }
+
+            return true;
+        } 
+
+        #endregion
+    }
+}
